f97de89e995db489c9e2b4853f3c59977d8d5fba
최성진 <81632633+csj0362@users.noreply.github.com>
2022-01-19 21:37:54 +0900

    1주차 문제풀이

--- 부스트코스 인공지능/1주차/find_string.py
+++ 부스트코스 인공지능/1주차/find_string.py
@@ -5,5 +5,4 @@ def find_string(inputs):
     newString = ''.join(i if not i.isdigit() else ' ' for i in inputs).split()
     return newString
 
-string_list = find_string(inputs)
-print(string_list)
+print(find_string(inputs))
