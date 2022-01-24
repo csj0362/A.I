f8acd836d201efa4b950bb89a90635af8807879e
최성진 <81632633+csj0362@users.noreply.github.com>
2022-01-24 11:47:57 +0900

    수정 2

--- 동의대 인공지능/7장/7.8.py
+++ 동의대 인공지능/7장/7.8.py
@@ -5,14 +5,43 @@ if num > 10 or num < 1:
         num = int(input('n을 입력하시오 : '))
         if num >= 1 and num <= 10:
             break
-
 numbers = list(range(1, num*num+1))
 
+result_list = []
 j = 0
 for i in range(num):
     if i % 2 == 0:
-        print('  '.join(map(str, numbers[j:j+num])), end = ' ') 
+        result_list.append(numbers[j:j+num])
+        #print(k)
     else:
-        print('  '.join(map(str, numbers[j+(num-1):j-1:-1])), end = ' ') 
+        result_list.append(numbers[j+(num-1):j-1:-1])
+        #print(f'{" ".join(map(str, numbers[j+(num-1):j-1:-1])):<13}')
     j = j + num 
+
+for i in result_list:
+    for j in i:
+        if j < 10:
+            print(' {:<2}'.format(j), end = ' ')
+        else:
+            print('{:<3}'.format(j), end = ' ')
     print()
+
+# j = 0
+# for i in range(num):
+#     if i % 2 == 0:
+#         print('  '.join(map(str, numbers[j:j+num])), end = ' ') 
+#     else:
+#         print('  '.join(map(str, numbers[j+(num-1):j-1:-1])), end = ' ') 
+#     j = j + num 
+#     print()
+
+# for i in range(num):
+#     if i == 0:
+#         print('0번째')    
+#     else:
+#         count += 1
+#         for j in range(num):
+#             if i % 2 == 0:
+#                 print('짝수 : ', numbers[num * count + 1:num * (count+1)+2:1], end = ' ')       
+#             else:
+#                 print('홀수: ', numbers[num * count + 1:num * (count+1)+1:1], end = ' ')
